7ebeb4ca935037b646e8625646fde95882cdd979
Rija Muhamad Yazid <rijamyazid@gmail.com>
2021-11-01 22:08:28 +0700

    Finish: Uploaded

--- app/src/main/java/com/example/githubuser/datasource/remote/RemoteSealed.kt
+++
@@ -1,6 +0,0 @@
-package com.example.githubuser.datasource.remote
-
-sealed class RemoteSealed<out T> {
-    data class Value<out T>(val data: T) : RemoteSealed<T>()
-    data class Error(val message: String?) : RemoteSealed<Nothing>()
-}
--- app/src/main/java/com/example/githubuser/datasource/remote/api/ApiService.kt
+++ app/src/main/java/com/example/githubuser/datasource/remote/api/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.githubuser.datasource.remote.api
 
+import com.example.githubuser.BuildConfig
 import com.example.githubuser.datasource.remote.response.SearchResponse
 import com.example.githubuser.datasource.remote.response.UserDetailResponse
 import com.example.githubuser.datasource.remote.response.UserResponse
@@ -17,36 +18,36 @@ interface ApiService {
 
     @GET(LIST_USER)
     @Headers(
-        "Authorization: token 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",
-        "user-agent: rijamyazid"
+        "Authorization: token ${BuildConfig.GITHUB_TOKEN}",
+        "user-agent: ${BuildConfig.GITHUB_USER_AGENT}"
     )
     suspend fun getUsers(): List<UserResponse>
 
     @GET("$DETAIL_USER{login}")
     @Headers(
-        "Authorization: token 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",
-        "user-agent: rijamyazid"
+        "Authorization: token ${BuildConfig.GITHUB_TOKEN}",
+        "user-agent: ${BuildConfig.GITHUB_USER_AGENT}"
     )
     suspend fun getUserDetail(@Path("login") username: String): UserDetailResponse
 
     @GET(SEARCH_USER)
     @Headers(
-        "Authorization: token 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",
-        "user-agent: rijamyazid"
+        "Authorization: token ${BuildConfig.GITHUB_TOKEN}",
+        "user-agent: ${BuildConfig.GITHUB_USER_AGENT}"
     )
     suspend fun getUsersByQuery(@Query("q") query: String): SearchResponse
 
     @GET("$DETAIL_USER{login}/$FOLLOWERS_USER")
     @Headers(
-        "Authorization: token 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",
-        "user-agent: rijamyazid"
+        "Authorization: token ${BuildConfig.GITHUB_TOKEN}",
+        "user-agent: ${BuildConfig.GITHUB_USER_AGENT}"
     )
     suspend fun getUserFollowers(@Path("login") username: String): List<UserResponse>
 
     @GET("$DETAIL_USER{login}/$FOLLOWING_USER")
     @Headers(
-        "Authorization: token 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",
-        "user-agent: rijamyazid"
+        "Authorization: token ${BuildConfig.GITHUB_TOKEN}",
+        "user-agent: ${BuildConfig.GITHUB_USER_AGENT}"
     )
     suspend fun getUserFollowing(@Path("login") username: String): List<UserResponse>
 
